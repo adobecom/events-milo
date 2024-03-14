6168f02a6d58e2bb8e607fbe118a08119b30cd0a
Qiyun Dai <pianoquin@gmail.com>
2024-03-14 11:54:26 -0500

    fetch approach working

--- blocks/page-server/page-server.js
+++ blocks/page-server/page-server.js
@@ -1,65 +1,37 @@
-import HtmlSanitizer from '../../deps/html-sanitizer.js';
-import { getMetadata, yieldToMain } from '../../utils/utils.js';
+import { getMetadata } from '../../utils/utils.js';
 
-const ignoredMeta = [
-  'serp-content-type',
-  'description',
-  'primaryproductname',
-  'theme',
-  'show-free-plan',
-  'sheet-powered',
-  'viewport',
-];
-
-async function sanitizeMeta(meta) {
-  if (meta.property || meta.name.includes(':') || ignoredMeta.includes(meta.name)) return;
-  await yieldToMain();
-  meta.content = HtmlSanitizer.SanitizeHtml(meta.content);
-}
-
-// metadata -> dom blades
+// data -> dom gills
 async function autoUpdatePage(main) {
   if (!main) {
     window.lana?.log('page server block cannot find it\'s parent main');
     return;
   }
 
-  const metaTags = document.head.querySelectorAll('meta');
-
-  await Promise.all(Array.from(metaTags).map((meta) => sanitizeMeta(meta)));
-
-  // main.innerHTML = main.innerHTML.replaceAll(regex, (_match, p1) => {
-  //   console.log(getMetadata(p1), p1);
-  //   return getMetadata(p1);
-  // });
-
-  // Select all elements in the document
-  const allElements = main.querySelectorAll('*');
-
-  // Define a regular expression to find [[ ]] content
-  const bracketRegex = /\[\[(.*?)\]\]/g;
-
-  // Iterate over all elements
-  allElements.forEach((element) => {
-    // Check if the element has text nodes
-    if (element.childNodes.length) {
-      element.childNodes.forEach((child) => {
-        // Make sure we're dealing with a text node
-        if (child.nodeType === 3) {
-          const originalText = child.nodeValue;
-          // Replace the content inside [[ ]] with the replacementText
-          const replacedText = originalText.replace(bracketRegex, (_match, p1) => {
-            console.log(getMetadata(p1), p1);
-            return getMetadata(p1);
-          });
-          if (replacedText !== originalText) {
-            // Update the text node only if changes were made
-            child.nodeValue = replacedText;
-          }
-        }
-      });
+  const json = await fetch('/t3/event/default/metadata.json').then((resp) => {
+    if (resp.ok) {
+      return resp.json();
     }
+
+    window.lana?.log('Error while attempting to fetch /t3/event/default/metadata.json');
+    return null;
   });
+
+  if (json) {
+    const pageData = json.data.find((d) => d.url === window.location.pathname);
+    const allElements = main.querySelectorAll('*');
+    const bracketRegex = /\[\[(.*?)\]\]/g;
+    allElements.forEach((element) => {
+      if (element.childNodes.length) {
+        element.childNodes.forEach((child) => {
+          if (child.nodeType === 3) {
+            const originalText = child.nodeValue;
+            const replacedText = originalText.replace(bracketRegex, (_match, p1) => pageData[p1] || '');
+            if (replacedText !== originalText) child.nodeValue = replacedText;
+          }
+        });
+      }
+    });
+  }
 
   // handle link replacement
   main.querySelectorAll('a[href*="#"]').forEach((a) => {
@@ -75,6 +47,6 @@ async function autoUpdatePage(main) {
   });
 }
 
-export default function init(el) {
-  autoUpdatePage(el.closest('main'));
+export default async function init(el) {
+  await autoUpdatePage(el.closest('main'));
 }
