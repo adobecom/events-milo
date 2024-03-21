9f01dcb9c182da6fc97a743277fa4370ba580c6d
Qiyun Dai <pianoquin@gmail.com>
2024-03-21 10:12:10 -0500

    add console log for debugging

--- blocks/page-server/page-server.js
+++ blocks/page-server/page-server.js
@@ -56,6 +56,7 @@ async function autoUpdatePage(main, hash) {
     }
 
     const res = flattenObject(pageData);
+    console.log(res);
     const findRegexMatch = (_match, p1) => res[p1] || '';
     const allElements = main.querySelectorAll('*');
     const reg = /\[\[(.*?)\]\]/g;
