af1704c060f38ef2fd72cbac434c74557334a959
Qiyun Dai <pianoquin@gmail.com>
2024-05-17 12:30:59 -0500

    event map finished

--- blocks/event-map/event-map.js
+++ blocks/event-map/event-map.js
@@ -3,8 +3,6 @@ import { getLibs } from '../../scripts/utils.js';
 const { createTag } = await import(`${getLibs()}/utils/utils.js`);
 
 const ENCODED_API_KEY = 'QUl6YVN5RHZ5cXdhVXMtSXZNS1BTb3RkV2JVRFJETmtUbkhXMlpB';
-const SIGNATURE = 'strata';
-const GOOGLE_MAP_ID = 'd3555ecb8ace8a82';
 
 function loadMapboxConfigs(el) {
   const configs = {};
@@ -13,8 +11,8 @@ function loadMapboxConfigs(el) {
   if (!configsDiv) return null;
 
   Array.from(configsDiv.children).forEach((col, i) => {
-    if (i === 0) configs.mapStyle = col.textContent.trim();
-    if (i === 1) configs.coordinates = col.textContent.split(',');
+    if (i === 0) configs.mapId = col.textContent.trim();
+    if (i === 1) configs.coordinates = col.textContent;
     if (i === 2) configs.zoom = parseInt(col.textContent.trim(), 10);
   });
 
@@ -22,20 +20,36 @@ function loadMapboxConfigs(el) {
   return configs;
 }
 
-function decorateMapContainer(configs) {
+function decorateTextContainer(el) {
+  const wrapper = el.querySelector('.event-map-wrapper');
+  const textContentWrapper = el.querySelector(':scope > div:first-of-type > div');
+
+  if (!textContentWrapper) return;
+
+  textContentWrapper.classList.add('text-wrapper');
+  wrapper.append(textContentWrapper);
+}
+
+function decorateMapContainer(el, configs) {
+  const wrapper = el.querySelector('.event-map-wrapper');
+
   const { mapContainer } = configs;
   mapContainer.innerHTML = '';
   mapContainer.classList.add('map-container');
   mapContainer.id = 'map-container';
 
-  const coordString = `${configs.coordinates[1]},${configs.coordinates[0]}`;
-  const img = createTag('img', { src: `https://maps.googleapis.com/maps/api/staticmap?map_id=${GOOGLE_MAP_ID}&center=${coordString}&zoom=${configs.zoom}&size=600x400&key=${window.atob(ENCODED_API_KEY)}&markers=color:red%7C${coordString}` });
+  const img = createTag('img', { src: `https://maps.googleapis.com/maps/api/staticmap?map_id=${configs.mapId}&center=${configs.coordinates}&zoom=${configs.zoom}&size=600x400&key=${window.atob(ENCODED_API_KEY)}&markers=color:red%7C${configs.coordinates}` });
   mapContainer.append(img);
+  wrapper.append(mapContainer);
 }
 
 export default async function init(el) {
   const configs = loadMapboxConfigs(el);
   if (!configs) return;
 
-  decorateMapContainer(configs);
+  const wrapper = createTag('div', { class: 'event-map-wrapper' });
+  el.append(wrapper);
+
+  decorateTextContainer(el);
+  decorateMapContainer(el, configs);
 }
