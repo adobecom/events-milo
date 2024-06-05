6593157f3e2a30c94a3091f25e71023b22f08057
Qiyun Dai <pianoquin@gmail.com>
2024-06-05 15:35:48 -0500

    Update event-partners.js

--- blocks/event-partners/event-partners.js
+++ blocks/event-partners/event-partners.js
@@ -1,40 +1,22 @@
 import { getLibs } from '../../scripts/utils.js';
 
-const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+const { createTag, getMetadata } = await import(`${getLibs()}/utils/utils.js`);
 
-function getMetadata(name, doc = document) {
-    const attr = name && name.includes(':') ? 'property' : 'name';
-    const meta = doc.head.querySelector(`meta[${attr}="${name}"]`);
-    return meta && meta.content;
-  }
+export default function init(el) {
+  const partnersData = JSON.parse(getMetadata('partners'));
 
-export default function init (el) {
-    console.log('init executed');
+  const eventPartnersSection = createTag('section', { class: 'event-partners images' });
 
-    const partnersData = JSON.parse(getMetadata('partners')); 
-    console.log('partners', partnersData);
+  partnersData.forEach((partner) => {
+    const aTag = createTag('a', { href: `${partner.externalLink}` }, '', { parent: eventPartnersSection });
 
-    const eventPartnersSection = createTag('section', { class: 'event-partners images'})
-    console.log('el', el);
-    console.log('eventPartnersSection', eventPartnersSection);
+    const article = createTag('article', { class: 'event-partners logo' }, '', { parent: aTag });
+    createTag('img', { src: `${partner.imageUrl}` }, '', { parent: article });
 
-    
-    // [...partnersData, ...partnersData].forEach((partner) => {
+    eventPartnersSection.append(aTag);
+  });
 
-    partnersData.forEach((partner) => {
-        const aTag = createTag('a', {href: `${partner.externalLink}`});
-
-        const article = createTag('article', { class: 'event-partners logo'});
-        aTag.append(article);
-        const img = createTag('img', {src: `${partner.imageUrl}`});
-        article.append(img);
-        console.log('partner.imageUrl', partner.imageUrl)
-        eventPartnersSection.append(aTag);
-    });
-
-
-    el.append(eventPartnersSection);
-
+  el.append(eventPartnersSection);
 }
 
 export { init };
