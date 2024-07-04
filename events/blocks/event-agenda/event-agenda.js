84658541df6fb41a7c747dd0e552a29569cf806f
Qiyun Dai <pianoquin@gmail.com>
2024-07-04 16:38:33 -0500

    typo

--- events/blocks/event-agenda/event-agenda.js
+++ events/blocks/event-agenda/event-agenda.js
@@ -38,7 +38,7 @@ export default async function init(el) {
     el.classList.add('blade');
     const h2 = el.querySelector('h2');
     agendaItemsCol.prepend(h2);
-    venueImageCol.append(createOptimizedPicture(venueImage.imgUrl, venueImage.altText || 'Venue Image'));
+    venueImageCol.append(createOptimizedPicture(venueImage.imageUrl, venueImage.altText || 'Venue Image'));
     container.append(venueImageCol);
   }
 
