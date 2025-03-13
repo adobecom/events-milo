f1f2921e9eaa8a281ca89bf612415b662e12933d
michaelready <michael@readyartwork.com>
2025-03-13 14:00:36 -0700

    MWPW-160887: add venue additional info fields to events detail (#269)
    * wip event details page venue additional modal
    
    * move class to decorate button
    
    * convert markdown to html and append to venue additional info modal
    
    * style venue additional rte modal
    
    * make venue modal larger
    
    * remove unused code
    
    * show venue additional info post event logic
    
    * await import and lint fixes
    
    * fix event map test
    
    ---------
    
    Co-authored-by: Michael Chan <sin52605@adobe.com>

--- events/blocks/event-map/event-map.js
+++ events/blocks/event-map/event-map.js
@@ -1,10 +1,9 @@
 import { LIBS, getMetadata } from '../../scripts/utils.js';
 
-const { createTag } = await import(`${LIBS}/utils/utils.js`);
-
-function decorateTextContainer(el) {
+function decorateTextContainer(el, createTag, decorateButtons) {
   const wrapper = el.querySelector('.event-map-wrapper');
   const textContentWrapper = el.querySelector(':scope > div:first-of-type > div');
+  const additionalInfo = el.querySelector(':scope > div:first-of-type > div > p:last-of-type:has(a)');
 
   if (!textContentWrapper) return;
 
@@ -33,9 +32,16 @@ function decorateTextContainer(el) {
     if (address) createTag('p', { class: 'venue-address-text' }, address, { parent: textContentWrapper });
     if (city && state && postalCode) createTag('p', { class: 'venue-address-text' }, `${city}, ${state} ${postalCode}`, { parent: textContentWrapper });
   }
+
+  if (getMetadata('show-venue-additional-info-post-event') !== 'true' && document.body.classList.contains('timing-post-event')) return;
+
+  if (additionalInfo) {
+    decorateButtons(additionalInfo, 'button-l');
+    textContentWrapper.append(additionalInfo);
+  }
 }
 
-function decorateMap(el) {
+function decorateMap(el, createTag) {
   let venueMapImageObj;
   try {
     venueMapImageObj = JSON.parse(getMetadata('photos')).find((photo) => photo.imageKind === 'venue-map-image');
@@ -74,14 +80,18 @@ function decorateMap(el) {
 }
 
 export default async function init(el) {
+  const [{ createTag }, { decorateButtons }] = await Promise.all([
+    import(`${LIBS}/utils/utils.js`),
+    import(`${LIBS}/utils/decorate.js`),
+  ]);
   if (getMetadata('show-venue-post-event') !== 'true' && document.body.classList.contains('timing-post-event')) {
     el.remove();
     return;
   }
 
-  const wrapper = createTag('div', { class: 'event-map-wrapper' });
+  const wrapper = createTag('div', { class: 'event-map-wrapper dark' });
   el.append(wrapper);
 
-  decorateTextContainer(el);
-  decorateMap(el);
+  decorateTextContainer(el, createTag, decorateButtons);
+  decorateMap(el, createTag);
 }
