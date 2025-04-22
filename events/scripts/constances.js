50645d3372c60da4644b13b53f0515c5c0e62920
Qiyun Dai <pianoquin@gmail.com>
2025-04-21 23:28:37 -0400

    Add content-update HTML support

--- events/scripts/constances.js
+++ events/scripts/constances.js
@@ -6,3 +6,9 @@ export const SUSI_OPTIONS = {
     prod: 'v:2,s,bg:milo,b719a8b0-6ba6-11ef-933e-7f38920b05fd',
   },
 };
+export const RTE_ATTRIBUTES = [
+  'rsvp-description',
+  'rich-description',
+  'agenda.details',
+  'venue.additionalVenueInformation',
+];
