7907c4e45b579520411ec4503ebf33299f5c5d5b
Gautam Bajaj <gautambajaj91@gmail.com>
2025-05-14 11:05:53 +0530

    fix changes

--- events/blocks/adobe-connect/adobe-connect.js
+++ events/blocks/adobe-connect/adobe-connect.js
@@ -4,7 +4,8 @@ const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
 export default async function init(el) {
   const h2 = el.querySelector('h2');
-  const url = h2?.textContent;
+  let url = h2?.textContent;
+  url += '&mkto_trk=abcs';
   h2.remove();
   createTag('iframe', { src: url, frameborder: '0', allowfullscreen: 'true', class: 'fullwidth' }, '', { parent: el });
 }
