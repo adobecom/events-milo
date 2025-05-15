90e810f52bc8e6a9acf912bc677478a93ba07ff7
Gautam Bajaj <gautambajaj91@gmail.com>
2025-05-15 22:29:44 +0530

    url based query param for connect.

--- events/blocks/adobe-connect/adobe-connect.js
+++ events/blocks/adobe-connect/adobe-connect.js
@@ -2,10 +2,34 @@ import { LIBS } from '../../scripts/utils.js';
 
 const { createTag } = await import(`${LIBS}/utils/utils.js`);
 
+function addParams(searchParams, params, key, value) {
+  if (params.has(key)) {
+    searchParams[key] = params.get(key);
+  } else {
+    searchParams[key] = value;
+  }
+}
+
+function addSearchParams(url, searchParams) {
+  const urlObj = new URL(url);
+  for (const [key, value] of Object.entries(searchParams)) {
+    urlObj.searchParams.append(key, value);
+  }
+  return urlObj.toString();
+}
+
 export default async function init(el) {
   const h2 = el.querySelector('h2');
   let url = h2?.textContent;
-  url += '&mkto_trk=abcs';
+  
+  const params = new URL(window.location.href).searchParams;
+  const searchParams = {};
+  addParams(searchParams, params, 'mkto_trk', 'marketo_tracker');
+  addParams(searchParams, params, 'mkt_tok', 'marketo_token');
+  addParams(searchParams, params, 'ecid', 'experience_cloud_id');
+  addParams(searchParams, params, 'mkto_event_id', 'marketo_event_id');
+
+  url = addSearchParams(url, searchParams);
   h2.remove();
   createTag('iframe', { src: url, frameborder: '0', allowfullscreen: 'true', class: 'fullwidth' }, '', { parent: el });
 }
