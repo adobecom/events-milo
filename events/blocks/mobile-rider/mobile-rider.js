ab2b2ebd7110ace9872d9975d73c34356c9cab53
hnvadobe <hnv@adobe.com>
2025-06-27 10:23:38 +0530

    change

--- events/blocks/mobile-rider/mobile-rider.js
+++ events/blocks/mobile-rider/mobile-rider.js
@@ -273,8 +273,8 @@ class MobileRider {
   
   setupASLButtonHandler(button, container) {
     button.addEventListener('click', () => {
-      if (!container?.classList?.contains(this.config.ASL.TOGGLE_CLASS)) {
-        container?.classList?.add(this.config.ASL.TOGGLE_CLASS);
+      if (!container?.classList?.contains(CONFIG.ASL.TOGGLE_CLASS)) {
+        container?.classList?.add(CONFIG.ASL.TOGGLE_CLASS);
         console.log('ASL enabled');
         this.initASL();
       }
