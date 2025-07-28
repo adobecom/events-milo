3d0de68cd8efcec3e5d2b0f12d07256760578c57
Gautam Bajaj <gautambajaj91@gmail.com>
2025-07-28 16:30:20 +0530

    adding new element

--- events/blocks/marketo-form/marketo-form.js
+++ events/blocks/marketo-form/marketo-form.js
@@ -1,10 +1,8 @@
-// import { LIBS } from '../../scripts/utils.js';
 import { CheckResourceLocation } from '../../../rs/360-KCI-804/images/mktoTestFormConfig.js';
 
-// const { createTag, getMetadata } = await import(`${LIBS}/utils/utils.js`);
-
 export default async function init(el) {
-  const resourceLocation = '.chrono-box';
+  const rows = Array.from(el.children);
+  const resourceLocation = `#${rows[0].textContent.trim().toLowerCase()}`;
   const resourceWatch = 'main .section .chrono-box';
   await CheckResourceLocation(el, resourceWatch, resourceLocation);
 }
