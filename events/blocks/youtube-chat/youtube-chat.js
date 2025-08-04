ba686009603412611e553c5a7c5236d1bd57f2c9
hnvadobe <hnv@adobe.com>
2025-08-04 14:36:45 +0530

    change

--- events/blocks/youtube-chat/youtube-chat.js
+++ events/blocks/youtube-chat/youtube-chat.js
@@ -21,7 +21,7 @@ const CONFIG = {
   },
   IFRAME_ATTRIBUTES: {
     allowfullscreen: true,
-    allow: 'accelerometer; autoplay; encrypted-media; gyroscope;',
+    allow: 'accelerometer; autoplay; encrypted-media; gyroscope',
   },
   DEFAULT_TITLE: 'YouTube video player',
   CHAT_LOAD_DELAY: 100,
