4e04bd3ee103ce7a8ade87d90ad9e9cbc3a05e03
Qiyun Dai <pianoquin@gmail.com>
2025-09-17 23:23:36 -0400

    fixing EVENT_LIBS

--- events/scripts/utils.js
+++ events/scripts/utils.js
@@ -10,13 +10,18 @@ export const EVENT_LIBS = (EVENT_CONFIG) => {
   const { version } = EVENT_CONFIG;
   const { hostname, search } = window.location;
 
-  let eventLibs = '/libs';
+  if (!(hostname.includes('.hlx.') || hostname.includes('.aem.') || hostname.includes('local'))) {
+    EVENT_CONFIG.eventLibs = '/event-libs';
+    return '/event-libs';
+  }
 
-  if (!(hostname.includes('.hlx.') || hostname.includes('.aem.') || hostname.includes('local'))) return eventLibs;
   const branch = new URLSearchParams(search).get('eventlibs') || 'main';
-  if (branch === 'local') eventLibs = `http://localhost:3868/event-libs/${version}`;
-  if (branch.includes('--')) eventLibs = `https://${branch}.aem.live/event-libs/${version}`;
-  if (branch.includes('--event-libs--adobecom.aem.live')) eventLibs = `https://${branch}--event-libs--adobecom.aem.live/event-libs/${version}`;
+  if (branch === 'local') {
+    EVENT_CONFIG.eventLibs = `http://localhost:3868/event-libs/${version}`;
+    return `http://localhost:3868/event-libs/${version}`;
+  }
+
+  const eventLibs = branch.includes('--') ? `https://${branch}.aem.live/event-libs/${version}` : `https://${branch}--event-libs--adobecom.aem.live/event-libs/${version}`;
   EVENT_CONFIG.eventLibs = eventLibs;
   return eventLibs;
 };
