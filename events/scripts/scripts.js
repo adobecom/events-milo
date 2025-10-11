44561f1e400dcb12f98a03947613ea810236d292
Vikrant Agarwal <vikranta@adobe.com>
2025-10-11 11:24:15 +0530

    testing

--- events/scripts/scripts.js
+++ events/scripts/scripts.js
@@ -264,6 +264,36 @@ if (getMetadata('event-details-page') === 'yes') await validatePageAndRedirect(L
  * ------------------------------------------------------------
  */
 
+async function getJumpToken(token) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-IMS-ClientId': 'CreativeCloud_v6_8',
+      'User-Agent': 'Creative Cloud',
+    },
+    body: new URLSearchParams({
+      bearer_token: token,
+      target_client_id: 'events-milo',
+      target_redirect_uri: 'https://www.stage.adobe.com/events.html',
+      target_scope: 'AdobeID,openid,gnav',
+      target_response_type: 'token',
+      locale: 'en_US',
+      client_id: 'CreativeCloud_v6_8',
+      target_remember_me: 'true',
+    }),
+  };
+
+  try {
+    const response = await fetch('https://adobeid-na1.services.adobe.com/ims/jumptoken/v1', options);
+    const data = await response.json();
+    console.log('========>', data);
+    return data;
+  } catch (error) {
+    console.error('=======err=>', error);
+    throw error;
+  }
+}
 (function loadStyles() {
   const paths = [`${LIBS}/styles/styles.css`];
   if (STYLES) { paths.push(STYLES); }
@@ -281,15 +311,20 @@ if (getMetadata('event-details-page') === 'yes') await validatePageAndRedirect(L
   let token = "";
   if(params.has('token')){
     token = params.get('token');
-  }else{
+  }else if(window.frameElement){
     const frameElement = window.frameElement;
-    token = frameElement.getAttribute("data-token");
+    token = frameElement?.getAttribute("data-token");
+  }else {
+    token = document.body.getAttribute('data-token');
   }
   if(token || localStorage.getItem("token")){
-    localStorage.setItem("token", params.get('token'));
+    localStorage.setItem("token", token || localStorage.getItem("token"));
     document.getElementsByTagName('header')[0].style.display = 'none';
     document.getElementsByTagName('footer')[0].style.display = 'none';
   }
+  const jumpToken = await getJumpToken(token);
+  localStorage.setItem("token", jumpToken || token)
+  console.log("=====>",jumpToken)
   await loadLana({ clientId: 'events-milo' });
   await loadArea().then(() => {
     if (getMetadata('event-details-page') === 'yes') lazyCaptureProfile();
